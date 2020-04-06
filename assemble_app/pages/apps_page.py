d37c4f1e24e99f5fd6d6c64544d169dd9d0e99cc
Alekur333 <amastar963@gmail.com>
2020-04-06 10:02:30 +0500

    some improvements

--- assemble_app/pages/apps_page.py
+++ assemble_app/pages/apps_page.py
@@ -6,19 +6,26 @@ from selenium.webdriver.support import expected_conditions as ec
 class AppsPage(BasePage):
 
     def should_be_remove_app_btn(self):
-        assert WebDriverWait(self.browser, 10).until\
-            (ec.presence_of_element_located(AppDoneLocators.REMOVE_APP_BTN)), \
-            "Кнопка удалить 'Демо приложение' не найдена"
+        # assert self.browser.find_element(*AppDoneLocators.REMOVE_APP_BTN).click(),\
+        #     "Кнопка удалить 'Демо приложение' не найдена"
+        # assert WebDriverWait(self.browser, 10).until\
+        #     (ec.presence_of_element_located(AppDoneLocators.REMOVE_APP_BTN)), \
+        #     "Кнопка удалить 'Демо приложение' не найдена"
+        assert WebDriverWait(self.browser, 10).until \
+            (ec.element_to_be_clickable(AppDoneLocators.REMOVE_APP_BTN)), \
+            "Кнопка удалить 'Демо приложение' не найдена, возможно приложение не собрано"
+
 
     # def remove_app(self):
     #     self.browser.find_element(*AppDoneLocators.REMOVE_APP_BTN).click()
     def remove_app(self):
-        try:
-            WebDriverWait(self.browser, 3).until\
-                (ec.element_to_be_clickable(AppsFunctionsLocators.HELP_BUTTON_CLOSE)).click.\
-                find_element(*AppDoneLocators.REMOVE_APP_BTN).click()
-        except:
-            self.browser.find_element(*AppDoneLocators.REMOVE_APP_BTN).click()
+        self.browser.find_element(*AppDoneLocators.REMOVE_APP_BTN).click()
+        # try:
+        #     WebDriverWait(self.browser, 3).until\
+        #         (ec.element_to_be_clickable(AppsFunctionsLocators.HELP_BUTTON_CLOSE)).click.\
+        #         find_element(*AppDoneLocators.REMOVE_APP_BTN).click()
+        # except:
+        #     self.browser.find_element(*AppDoneLocators.REMOVE_APP_BTN).click()
 
     def should_be_remove_app_continue_btn(self):
         assert WebDriverWait(self.browser, 10).until\
